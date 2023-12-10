3b9b379325eeb6e0e19a8ec5629e11c384d7fe8a
EugeneTsalko <tefworkmail@gmail.com>
2023-12-10 13:40:06 +0300

    refactor: move error handlers to midlleware

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { routes } from './routes';
-import { DbError, ValidationError } from './errors/errors';
+import { errorHandler, notFoundHandler } from './middleware/errorHandling';
 
 const app = new Hono();
 app.route('/', routes);
 
-app.onError((err, c) => {
-  if (err instanceof DbError) {
-    return c.json({ ok: false, message: err.message }, err.code);
-  }
+app.onError(errorHandler);
 
-  if (err instanceof ValidationError) {
-    return c.json({ ok: false, message: err.message }, 400);
-  }
-  return c.json({ ok: false, message: err.message }, 500);
-});
-
-app.notFound((c) => {
-  return c.json({ ok: false, message: 'Oops, not found.' }, 404);
-});
+app.notFound(notFoundHandler);
 
 serve(app);
