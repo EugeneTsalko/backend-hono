aa4b73d4a3784e401563e82a92db366dbe3adad3
EugeneTsalko <tefworkmail@gmail.com>
2024-01-02 16:38:38 +0300

    refactor: add helper to comment service

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,11 +1,11 @@
-import { PrismaClient, type Comment } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { commentRepository } from '../dataAcces/commentRepository';
 import { userRepository } from '../dataAcces/userRepository';
 
 const prisma = new PrismaClient();
 
 export const commentService = {
-  async like(commentId: number, userId: number): Promise<Comment> {
+  async helper(commentId: number, userId: number) {
     const comment = await commentRepository.getById(commentId);
     const user = await userRepository.getById(userId);
 
@@ -38,6 +38,12 @@ export const commentService = {
         },
       });
     }
+
+    return { isAlreadyDisliked, isAlreadyLiked, comment };
+  },
+
+  async like(commentId: number, userId: number) {
+    const { isAlreadyLiked, comment } = await this.helper(commentId, userId);
 
     if (!isAlreadyLiked) {
       await prisma.likedComment.create({
@@ -45,41 +51,11 @@ export const commentService = {
       });
     }
 
-    return comment;
+    return { ...comment, likedBy: [...comment.likedBy, { userId, commentId }] };
   },
-  async dislike(commentId: number, userId: number): Promise<Comment> {
-    const comment = await commentRepository.getById(commentId);
-    const user = await userRepository.getById(userId);
 
-    const isAlreadyLiked = await prisma.likedComment.findFirst({
-      where: {
-        userId: user.id,
-        commentId: comment.id,
-      },
-    });
-
-    const isAlreadyDisliked = await prisma.dislikedComment.findFirst({
-      where: {
-        userId: user.id,
-        commentId: comment.id,
-      },
-    });
-
-    if (isAlreadyLiked) {
-      await prisma.likedComment.delete({
-        where: {
-          userId_commentId: { userId, commentId },
-        },
-      });
-    }
-
-    if (isAlreadyDisliked) {
-      await prisma.dislikedComment.delete({
-        where: {
-          userId_commentId: { userId, commentId },
-        },
-      });
-    }
+  async dislike(commentId: number, userId: number) {
+    const { isAlreadyDisliked, comment } = await this.helper(commentId, userId);
 
     if (!isAlreadyDisliked) {
       await prisma.dislikedComment.create({
@@ -87,6 +63,9 @@ export const commentService = {
       });
     }
 
-    return comment;
+    return {
+      ...comment,
+      dislikedBy: [...comment.dislikedBy, { userId, commentId }],
+    };
   },
 };
